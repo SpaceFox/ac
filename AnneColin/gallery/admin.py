830769012dcfa0a3574005cb06be43a039500482
spacefox <spacefox@hastur>
2014-03-09 22:24:55 +0100

    Admin

---
+++ AnneColin/gallery/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from gallery.models import Category, Format, Picture, Price
+
+admin.site.register(Category)
+admin.site.register(Format)
+admin.site.register(Picture)
+admin.site.register(Price)
