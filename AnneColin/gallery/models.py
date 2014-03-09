b45f1b20c4836cf43734d2e59f9815ba24ded4de
spacefox <spacefox@hastur>
2014-03-09 22:07:18 +0100

    Modèle

--- AnneColin/gallery/models.py
+++ AnneColin/gallery/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name        = models.CharField(max_length = 80)
+    slug        = models.SlugField(max_length = 80)
+    icon        = models.ImageField(upload_to = 'categories')
+    description = models.TextField(null = True)
+    pub_date    = models.DateTimeField(auto_now_add = True)
+
+class Format(models.Model):
+    name        = models.CharField(max_length = 80)
+    description = models.TextField(null = True)
+
+class Picture(models.Model):
+    category    = models.ForeignKey(Category)
+    formats     = models.ManyToManyField(Format, through = 'Price')
+    title       = models.CharField(max_length = 80)
+    slug        = models.SlugField(max_length = 80)
+    image       = models.ImageField(upload_to = 'pictures')
+    description = models.TextField(null = True)
+    pub_date    = models.DateTimeField(auto_now_add = True)
+    
+class Price(models.Model):
+    picture = models.ForeignKey(Picture)
+    format  = models.ForeignKey(Format)
+    value   = models.IntegerField()
