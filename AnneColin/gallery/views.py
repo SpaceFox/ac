97a3b3efa45576e02a46a5eb986764f468270c13
spacefox <spacefox@hastur>
2014-03-10 23:10:03 +0100

    un peu de FO

--- AnneColin/gallery/views.py
+++ AnneColin/gallery/views.py
@@ -1,1 +1,21 @@
+# coding: utf-8
+
 # Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import generic
+
+from gallery.models import Category
+
+class IndexView(generic.ListView):
+    template_name = 'gallery/index.html'
+    context_object_name = 'categories'
+    
+    def get_queryset(self):
+        return Category.objects.all()
+
+def category(request, category_id):
+    return HttpResponse("Affichage de la categorie %s" % category_id)
+
+def picture(request, picture_id):
+    return HttpResponse("Affichage de l'image %s" % picture_id)
