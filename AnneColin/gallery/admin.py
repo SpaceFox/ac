cb30ded2971838625fd71833013b54f6ced8e6a6
François Colin <fcolin@ecocea.com>
2014-04-21 01:42:13 +0200

    Remove sorl thumbnails (admin)

--- AnneColin/gallery/admin.py
+++ AnneColin/gallery/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from gallery.models import Category, Format, Picture, Price
-from sorl.thumbnail.admin import AdminImageMixin
 
-class CategoryAdmin(AdminImageMixin, admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'pub_date')
     pass
 
-class PictureAdmin(AdminImageMixin, admin.ModelAdmin):
+class PictureAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'pub_date')
     list_filter = ['category']
     pass
