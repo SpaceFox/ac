8767dfe60d3d4166662884125223d8fb44f72d65
SpaceFox <ujueseo.yeou@gmail.com>
2017-04-23 17:16:47 +0200

    Passage à Python 3

--- AnneColin/gallery/views.py
+++ AnneColin/gallery/views.py
@@ -1,5 +1,3 @@
-# coding: utf-8
-
 import datetime
 
 from django.shortcuts import render, get_object_or_404
@@ -15,8 +13,8 @@ from AnneColin import settings
 
 
 class IndexView(generic.ListView):
-    template_name = u'gallery/index.html'
-    context_object_name = u'categories'
+    template_name = 'gallery/index.html'
+    context_object_name = 'categories'
 
     def get_queryset(self):
         return Category.objects.all()
@@ -24,88 +22,88 @@ class IndexView(generic.ListView):
 
 def category(request, category_id):
     category = get_object_or_404(Category, pk=category_id)
-    categories = Category.objects.all().order_by(u'pub_date')
+    categories = Category.objects.all().order_by('pub_date')
 
-    previous_cat = Category.objects.filter(pub_date__lt=category.pub_date).order_by(u'-pub_date').first()
-    next_cat = Category.objects.filter(pub_date__gt=category.pub_date).order_by(u'pub_date').first()
+    previous_cat = Category.objects.filter(pub_date__lt=category.pub_date).order_by('-pub_date').first()
+    next_cat = Category.objects.filter(pub_date__gt=category.pub_date).order_by('pub_date').first()
 
     pictures = category.picture_set.all()
-    return render(request, u'gallery/category.html',
-                  {u'category': category,
-                   u'categories': categories,
-                   u'previous_cat': previous_cat,
-                   u'next_cat': next_cat,
-                   u'pictures': pictures,
+    return render(request, 'gallery/category.html',
+                  {'category': category,
+                   'categories': categories,
+                   'previous_cat': previous_cat,
+                   'next_cat': next_cat,
+                   'pictures': pictures,
                   })
 
 
 def picture(request, picture_id):
     picture = get_object_or_404(Picture, pk=picture_id)
-    pictures = picture.category.picture_set.all().order_by(u'pub_date')[:12]
+    pictures = picture.category.picture_set.all().order_by('pub_date')[:12]
 
     previous_pic = Picture.objects.filter(
         category__id=picture.category.id,
         pub_date__lt=picture.pub_date
-    ).order_by(u'-pub_date').first()
+    ).order_by('-pub_date').first()
     next_pic = Picture.objects.filter(
         category__id=picture.category.id,
         pub_date__gt=picture.pub_date
-    ).order_by(u'pub_date').first()
+    ).order_by('pub_date').first()
 
-    return render(request, u'gallery/picture.html',
+    return render(request, 'gallery/picture.html',
                   {
-                      u'picture': picture,
-                      u'pictures': pictures,
-                      u'previous_pic': previous_pic,
-                      u'next_pic': next_pic,
+                      'picture': picture,
+                      'pictures': pictures,
+                      'previous_pic': previous_pic,
+                      'next_pic': next_pic,
                   })
 
 
 def prices(request):
-    return render(request, u'gallery/prices.html', {u'prices': Format.objects.all()})
+    return render(request, 'gallery/prices.html', {'prices': Format.objects.all()})
 
 
 def contact(request):
-    if request.method == u"POST":
+    if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
             data = form.data
             context = {
-                u'name': data[u'name'],
-                u'email': data[u'email'],
-                u'subject': data[u'subject'],
-                u'message': data[u'message'],
+                'name': data['name'],
+                'email': data['email'],
+                'subject': data['subject'],
+                'message': data['message'],
             }
 
             # Send email
-            subject = data[u'subject']
-            from_email = u"{0} <{1}>".format(data[u'name'], context[u'email'])
-            message_html = get_template(u"gallery/email/contact.html").render({'context': Context(context)})
-            message_txt = get_template(u"gallery/email/contact.txt").render({'context': Context(context)})
+            subject = data['subject']
+            from_email = "{0} <{1}>".format(data['name'], context['email'])
+            message_html = get_template("gallery/email/contact.html").render({'context': Context(context)})
+            message_txt = get_template("gallery/email/contact.txt").render({'context': Context(context)})
             msg = EmailMultiAlternatives(
                 subject,
                 message_txt,
                 from_email,
                 [settings.MAIL_CONTACT])
-            msg.attach_alternative(message_html, u"text/html")
+            msg.attach_alternative(message_html, "text/html")
             msg_base = Message()
             msg_base.date = datetime.datetime.now().isoformat()
-            msg_base.name = data[u'name']
-            msg_base.email = data[u'email']
-            msg_base.subject = data[u'subject']
-            msg_base.message = data[u'message']
+            msg_base.name = data['name']
+            msg_base.email = data['email']
+            msg_base.subject = data['subject']
+            msg_base.message = data['message']
             msg_base.save()
             try:
                 msg.send()
-                messages.success(request, u"Votre message a bien été envoyé.")
+                messages.success(request, "Votre message a bien été envoyé.")
             except:
                 msg = None
-                messages.error(request, u"Une erreur est survenue.")
+                messages.error(request, "Une erreur est survenue.")
 
             # reset the form after successful validation
             form = ContactForm()
-        return render(request, u"gallery/contact.html", {u"form": form})
+        return render(request, "gallery/contact.html", {"form": form})
 
     form = ContactForm()
 
-    return render(request, u'gallery/contact.html', {u"form": form})
+    return render(request, 'gallery/contact.html', {"form": form})
